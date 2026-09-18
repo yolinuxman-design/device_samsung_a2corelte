7c12b7c4453f43a666103209509fc1035bf36e02
yolinuxman-design <yolinuxman@gmail.com>
2026-09-18 22:34:51 +0545

    Update lineage_a2corelte.mk

--- lineage_a2corelte.mk
+++ lineage_a2corelte.mk
@@ -15,7 +15,7 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
@@ -35,5 +35,4 @@ TARGET_VENDOR := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
-TARGET_BOOT_ANIMATION_RES := 720
-TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 480
