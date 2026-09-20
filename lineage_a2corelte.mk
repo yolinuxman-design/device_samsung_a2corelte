cf13d53aa560cc44905b188a672ae2c41166a24f
yolinuxman-design <yolinuxman@gmail.com>
2026-09-20 13:42:37 +0545

    Update lineage_a2corelte.mk

--- lineage_a2corelte.mk
+++ lineage_a2corelte.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/a2corelte/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_mini_phone.mk)
 
 PRODUCT_LOCALES := en_US en_GB
 
